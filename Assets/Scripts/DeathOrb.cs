99ba4d6de5aaa201ddb566425db4b1169d266375
Piotr <piter1997_21@wp.pl>
2023-05-23 18:02:46 +0200

    making death orb destroyable

--- Assets/Scripts/DeathOrb.cs
+++ Assets/Scripts/DeathOrb.cs
@@ -10,29 +10,44 @@ public class DeathOrb : MonoBehaviour
     private BreakableObject breakableObject;
     [SerializeField]
     private Color endColor;
+    [SerializeField]
+    private Rigidbody moveablePart;
+    [SerializeField]
+    private float moveablePartVelocity = 5f;
 
     private Color startingColor;
 
     private void Awake()
     {
         breakableObject.ObjectDamaged += ChangeColor;
-        startingColor = materialToChange.sharedMaterials[1].color;
-    }
-
-    private void Update()
-    {
-
+        breakableObject.ObjectBreaked += OpenOrb;
+        startingColor = materialToChange.materials[1].color;//TODO: magic number
     }
 
     private void OnDestroy()
     {
         breakableObject.ObjectDamaged -= ChangeColor;
+        breakableObject.ObjectBreaked -= OpenOrb;
     }
 
     private void ChangeColor()
     {
-        var lerpedColor = Color.Lerp(startingColor, endColor, .3f);
-        GetComponentInChildren<Renderer>().sharedMaterials[1].color = lerpedColor;
-        startingColor = gameObject.GetComponentInChildren<Renderer>().sharedMaterials[1].color;
+        var lerpedColor = Color.Lerp(startingColor, endColor, .5f);
+        materialToChange.materials[1].color = lerpedColor;
+        startingColor = materialToChange.materials[1].color;
+    }
+
+    private void OpenOrb()
+    {
+        Debug.Log("£OTWIEROJ PANOCKU");
+        moveablePart.useGravity = true;
+        moveablePart.velocity = transform.up * moveablePartVelocity;
+        StartCoroutine(HideUpperOrbPart());
+    }
+
+    IEnumerator HideUpperOrbPart()
+    {
+        yield return new WaitForSeconds(1f);
+        moveablePart.gameObject.SetActive(false);
     }
 }
